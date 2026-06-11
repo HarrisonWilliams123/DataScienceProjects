87c90bae3cbfc656308ee268a838880582639166
HarrisonWilliams123 <harrisonwms@gmail.com>
2026-06-11 15:15:54 -0400

    Created the Current NBA player's college stat sheet

--- NBACareerPredictor/dataCollection.py
+++ NBACareerPredictor/dataCollection.py
@@ -4,7 +4,22 @@ import time
 from nba_api.stats.endpoints import commonplayerinfo, playercareerstats
 from nba_api.stats.static import players
 
-players = players.get_active_players()
+active_players = players.get_active_players()
 
+nba_college_stats = pd.read_csv("data/college_seasons.csv")
+
+df_list = []
+
+for player in active_players:
+    full_name = player['full_name']
+    try:
+        df = nba_college_stats[nba_college_stats["player"] == full_name]
+        df_list.append(df)
+    except Exception as e:
+        print(f"Error finding college stats for {full_name}, {e}")
+    time.sleep(0.6)
+
+final_df = pd.concat(df_list, ignore_index=True)
+final_df.to_csv("data/NBAPlayerCollegeStats.csv", index=False)
 
 
